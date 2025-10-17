824c84fd91263f14ccdbed7244747beac2aa72b1
Alem <alem.bristric@gmail.com>
2025-10-17 07:26:48 +0200

    Added .gitignore and untrack  node_modules

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const placesRoutes = require("./routes/places-routes");
+
+const app = express();
+app.use(express.json());
+app.use("/api/places", placesRoutes);
+
+// app.use((error,  req, res, next) => {
+
+//     if(res.headerSent){
+//         return next(error);
+//      }
+//    res.status(error.code ||500);
+//    res.json({message: error.message || 'An unkown error occured' });
+
+// });
+
+app.use((error, req, res, next) => {
+  // Kolla om svaret redan skickats
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  // Sätt statuskod – default 500
+  const status = error.code || 500;
+
+  // Logga bara meddelandet i konsolen (inte hela objektet)
+  console.error(`[${status}] ${error.message}`);
+
+  // Skicka endast ett rent JSON-svar till klienten
+  res.status(status).json({
+    message: error.message || "An unknown error occurred",
+  });
+});
+
+app.listen(5000, () => console.log("Server running on :5000"));
